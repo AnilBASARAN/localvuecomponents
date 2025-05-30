37061f6850cb6e830aaecf5905eb4c5145e3127b
AnilBASARAN <nlbsrn@gmail.com>
2025-05-30 22:54:00 +0300

    refs used to alert user

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,6 +1,21 @@
 <template>
     <div>
         <h2>Manage Goals</h2>
-        <input type="text" />
+        <input type="text" ref="goal" />
+        <button @click="setGoal">Set Goal</button>
     </div>
 </template>
+
+<script>
+
+export default {
+    methods:{
+        setGoal() {
+        const enteredValue = this.$refs.goal.value;
+        if(enteredValue === ""){
+            alert("Input must not be empty!");
+        }
+    },
+    }
+}
+</script>
