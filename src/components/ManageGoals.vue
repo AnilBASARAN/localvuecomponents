593123b89317ee75991f9fe9aeb4c0855752c2e1
AnilBASARAN <nlbsrn@gmail.com>
2025-05-30 23:14:07 +0300

    alert modal

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,21 +1,37 @@
 <template>
-    <div>
-        <h2>Manage Goals</h2>
-        <input type="text" ref="goal" />
-        <button @click="setGoal">Set Goal</button>
-    </div>
+  <div>
+    <h2>Manage Goals</h2>
+    <input type="text" v-model="enteredValue" />
+    <button @click="setGoal">Set Goal</button>
+
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is invalid!</h2>
+      <p>Please enter at least 2 characters...</p>
+    </error-alert>
+  </div>
 </template>
 
 <script>
+import ErrorAlert from "./ErrorAlert.vue";
 
 export default {
-    methods:{
-        setGoal() {
-        const enteredValue = this.$refs.goal.value;
-        if(enteredValue === ""){
-            alert("Input must not be empty!");
-        }
-    },
+  components: { ErrorAlert },
+  data() {
+    return {
+      enteredValue: "",
+      inputIsInvalid: false
+    };
+  },
+  methods: {
+    setGoal() {
+      if (!this.enteredValue || this.enteredValue.trim().length < 2) {
+        this.inputIsInvalid = true;
+        return;
+      }
+      this.inputIsInvalid = false;
+      // handle your new goal here...
+      this.enteredValue = "";
     }
-}
+  }
+};
 </script>
